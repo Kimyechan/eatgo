b6c515ba365456569822e0c2ee9823ec098bc65c
Kimyechan <vlvkcjswo7@naver.com>
2020-09-03 17:46:37 +0900

    Delete some part from customer api
    delete need for admin api part from customer api project
    not need for customer api

--- eatgo-customer-api/src/main/java/com/example/eatgo/interfaces/RestaurantController.java
+++ eatgo-customer-api/src/main/java/com/example/eatgo/interfaces/RestaurantController.java
@@ -32,27 +32,5 @@ public class RestaurantController {
 
         return restaurant;
     }
-
-    @PostMapping("/restaurants")
-    public ResponseEntity<?> create(@Valid @RequestBody Restaurant resource) throws URISyntaxException {
-        String name = resource.getName();
-        String address = resource.getAddress();
-        Restaurant restaurant = restaurantService.addRestaurant(
-                Restaurant.builder()
-                    .name(name)
-                    .address(address)
-                    .build());
-
-        URI location = new URI("/restaurants/" + restaurant.getId());
-        return ResponseEntity.created(location).body("{}");
-    }
-
-    @PatchMapping("/restaurants/{id}")
-    public String update(@PathVariable("id") Long id, @Valid @RequestBody Restaurant resource){
-        String name = resource.getName();
-        String address = resource.getAddress();
-        restaurantService.updateRestaurant(id, name, address);
-        return "{}";
-    }
 }
 
