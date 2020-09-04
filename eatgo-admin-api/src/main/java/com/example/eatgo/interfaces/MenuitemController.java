62462cfb5ae6236deb8e257cd0e2caa5ad9e45d6
Kimyechan <vlvkcjswo7@naver.com>
2020-09-04 12:50:21 +0900

    Configuration: change to h2 database and add setting enviroment for testing process
    change mode inMemory to file for making data persistence in h2 database
    and add testing enviroment SPRING_PROFILES_ACTIVE=test to make testing on inMemory h2

--- eatgo-admin-api/src/main/java/com/example/eatgo/interfaces/MenuitemController.java
+++ eatgo-admin-api/src/main/java/com/example/eatgo/interfaces/MenuitemController.java
@@ -3,12 +3,8 @@ package com.example.eatgo.interfaces;
 import com.example.eatgo.domain.MenuItem;
 import com.example.eatgo.service.MenuitemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,6 +12,13 @@ public class MenuitemController {
 
     @Autowired
     private MenuitemService menuitemService;
+
+    @GetMapping("/restaurants/{restaurantId}/menuitems")
+    public List<MenuItem> list(@PathVariable Long restaurantId) {
+        List<MenuItem> menuItems = menuitemService.getMenuItems(restaurantId);
+
+        return menuItems;
+    }
 
     @PatchMapping("/restaurants/{restaurantId}/menuitems")
     public String bulkUpdate(@PathVariable("restaurantId") Long restaurantId, @RequestBody List<MenuItem> menuItems){
