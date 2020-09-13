26042612c3fd36f26f96f67534b2b9f7d89efba5
Kimyechan <vlvkcjswo7@naver.com>
2020-09-14 00:01:13 +0900

    Fix: Not commit file for previous commit

--- eatgo-common/src/main/java/com/example/eatgo/utils/JwtUtil.java
+++ eatgo-common/src/main/java/com/example/eatgo/utils/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.example.eatgo.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -19,12 +20,17 @@ public class JwtUtil {
 
     public String createToken(Long userId, String name) {
         // TODO: JJWT 사용
-        String token = Jwts.builder()
+        return Jwts.builder()
                 .claim("userId", userId)
                 .claim("name", name)
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
+    }
 
-        return token;
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
--- eatgo-customer-api/src/main/java/com/example/eatgo/SecurityJavaConfig.java
+++ eatgo-customer-api/src/main/java/com/example/eatgo/SecurityJavaConfig.java
@@ -1,5 +1,6 @@
 package com.example.eatgo;
 
+import com.example.eatgo.filters.JwtAuthenticationFilter;
 import com.example.eatgo.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -7,8 +8,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.servlet.Filter;
+
 
 @Configuration
 @EnableWebSecurity
@@ -19,11 +24,17 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        Filter filter = new JwtAuthenticationFilter(authenticationManager(), jwtUtil());
+
         http
                 .cors().disable()
                 .csrf().disable()
                 .formLogin().disable()
-        .headers().frameOptions().disable();
+                .headers().frameOptions().disable()
+                .and()
+                .addFilter(filter)
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
