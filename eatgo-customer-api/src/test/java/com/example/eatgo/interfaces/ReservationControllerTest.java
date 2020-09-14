95d481e2172dc9a7cbd59eec2b51968e93019acb
Kimyechan <vlvkcjswo7@naver.com>
2020-09-14 22:18:38 +0900

    Feat: Reservation Service
    create service for saving reservation

--- eatgo-customer-api/src/test/java/com/example/eatgo/interfaces/ReservationControllerTest.java
+++ eatgo-customer-api/src/test/java/com/example/eatgo/interfaces/ReservationControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.eatgo.interfaces;
 
+import com.example.eatgo.domain.Reservation;
 import com.example.eatgo.service.ReservationService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,7 +11,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,11 +30,8 @@ public class ReservationControllerTest {
 
     @Test
     public void create() throws Exception {
-        Long userId = 1004L;
-        String name = "John";
-        String date = "2019-12-24";
-        String time = "20:00";
-        Integer partySize = 20;
+        Reservation mockReservation = Reservation.builder().id(12L).build();
+        given(reservationService.addReservation(any(), any(), any(), any(), any(), any())).willReturn(mockReservation);
 
         String token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEwMDQsIm5hbWUiOiJKb2huIn0.8hm6ZOJykSINHxL-rf0yV882fApL3hyQ9-WGlJUyo2A";
 
@@ -41,6 +41,11 @@ public class ReservationControllerTest {
                 .content("{ \"date\": \"2019-12-24\", \"time\": \"20:00\", \"partySize\": 20}"))
                 .andExpect(status().isCreated());
 
+        Long userId = 1004L;
+        String name = "John";
+        String date = "2019-12-24";
+        String time = "20:00";
+        Integer partySize = 20;
 
         verify(reservationService).addReservation(eq(369L), eq(userId), eq(name), eq(date), eq(time), eq(partySize));
     }
